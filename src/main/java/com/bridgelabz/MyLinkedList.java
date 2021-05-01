61268abc45465de69d32763a6d317f10f5ec8885
shuchitasingh <singhshuchita5@gmail.com>
2021-05-01 19:01:03 +0530

    Added:append elements

--- src/main/java/com/bridgelabz/MyLinkedList.java
+++ src/main/java/com/bridgelabz/MyLinkedList.java
@@ -44,13 +44,4 @@ public class MyLinkedList {
         }
     }
 
-    public void insert(INode myNode, INode newNode) {
-        INode tempNode=myNode.getNext();
-        myNode.setNext(newNode);
-        newNode.setNext(tempNode);
-    }
-
-
-
-
 }
